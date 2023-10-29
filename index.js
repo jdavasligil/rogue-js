58a8616e020c1dbe0cf284aac543680f31e1c599
jdavasligil <22987230+jdavasligil@users.noreply.github.com>
2023-10-28 22:13:58 -0700

    Add seedable RNG

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-var canvas = document.getElementById("game-canvas");
-var text = document.getElementById("game-text");
-var ctx = canvas.getContext("2d");
-var canvas_width = 896;
-var canvas_height = 504;
+const canvas = document.getElementById("game-canvas");
+const text = document.getElementById("game-text");
+const ctx = canvas.getContext("2d");
+const canvas_width = 896;
+const canvas_height = 504;
+const seed = 12345;
 
-function update_text(s) {
-  text.innerText = s
+function mulberry32(a) {
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
 }
 
-update_text("Rhoncus aenean vel elit scelerisque. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Malesuada fames ac turpis egestas maecenas pharetra. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Sit amet luctus venenatis lectus magna fringilla. Ante metus dictum at tempor commodo. Mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et. Purus ut faucibus pulvinar elementum integer enim neque. Tincidunt nunc pulvinar sapien et ligula. Viverra justo nec ultrices dui sapien eget mi proin. Tellus pellentesque eu tincidunt tortor aliquam. Nisl suscipit adipiscing bibendum est. Ultrices neque ornare aenean euismod elementum. At varius vel pharetra vel turpis nunc eget. Faucibus scelerisque eleifend donec pretium vulputate sapien nec. Porta non pulvinar neque laoreet. Viverra vitae congue eu consequat ac felis donec. Velit euismod in pellentesque massa placerat duis ultricies. Amet consectetur adipiscing elit duis tristique sollicitudin nibh sit. Facilisi morbi tempus iaculis urna id. A erat nam at lectus urna duis convallis. Non odio euismod lacinia at quis risus sed vulputate. Nisi porta lorem mollis aliquam ut porttitor. Diam phasellus vestibulum lorem sed risus ultricies tristique nulla. Eget gravida cum sociis natoque penatibus. Diam ut venenatis tellus in. Suspendisse sed nisi lacus sed viverra tellus. Proin libero nunc consequat interdum. Auctor urna nunc id cursus metus aliquam. Vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere. Lectus sit amet est placerat in. Ultrices sagittis orci a scelerisque purus semper eget duis at. Ultricies integer quis auctor elit. Id volutpat lacus laoreet non curabitur gravida arcu. Risus at ultrices mi tempus imperdiet nulla malesuada. Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Ultrices dui sapien eget mi proin sed libero enim. Ac tortor dignissim convallis aenean et tortor at risus viverra.")
+const rng = mulberry32(seed);
+
+function roll(n,m) {
+  total = 0
+  for (let i = 0; i < n; i++) {
+    total += Math.round(1 + rng() * (m - 1));
+  }
+
+  return total
+}
+
+function set_text(s) {
+  text.innerText = s;
+}
+function append_text(s) {
+  text.innerText += "\n\n" + s;
+}
+function write_description(s) {
+  text.innerHTML += "<p style='color: yellow;'>" + s + "</p><br />";
+}
+function write_action(s) {
+  text.innerHTML += "<p style='color: red;'>" + s + "</p><br />";
+}
+function clear_text() {
+  text.innerHTML = "";
+}
+
+write_description("The green slime appears sentient. It smells terribly strong of ammonia.");
+write_action("The slime attacks you!");
+
 ctx.moveTo(0, 0);
 ctx.lineTo(canvas_width, canvas_height);
+ctx.strokeStyle = "white"
 ctx.stroke();
